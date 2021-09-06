f2c9632043b7e8fcad1ef6d89522677ec91aafed
PremKSU <pkurumpa@students.kennesaw.edu>
2021-09-06 09:34:55 -0400

    Updated RX/TX python scripts
    Use "pytx.py" and "pyrx.py" for testing

--- Python Scripts/RX/rxpy.py
+++ Python Scripts/RX/rxpy.py
@@ -3,6 +3,11 @@ import socket
 ETH_P_ALL=0x88DC # not defined in socket module, sadly...
 s=socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
 s.bind(("wlp1s0", 0))
-r=s.recv(2000)
+pktnum = 1
+
 #sys.stdout.write("<%s>\n"%repr(r))
-print("%s" % r)
+while True:
+    r=s.recv(2000)
+    print("Received packet %i" % pktnum)
+    pktnum = pktnum+1
+    #print("%s" % r)
