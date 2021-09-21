46b871536076ed6a34d6dc073082ab906c5d1fd9
PremKSU <pkurumpa@students.kennesaw.edu>
2021-09-20 21:31:24 -0400

    Created TXRX for Py
    Created TXRX for Python Runtime

---
+++ py/rx/pyrxtest.py
@@ -0,0 +1,22 @@
+import sys
+import socket
+import signal
+ETH_P_ALL=0x88DC # not defined in socket module, sadly...
+s=socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+s.bind(("wlp1s0", 0))
+pktnum = 1
+
+#sys.stdout.write("<%s>\n"%repr(r))
+while True:
+    r=s.recv(2000)
+    print("Received packet %i" % pktnum)
+    pktnum = pktnum+1
+    #print("%s" % r)
+
+
+def signal_handler(sig, frame):
+	#print('\r\nNumber Rx: ')
+	print('\r\n'+len(pkt))
+	sys.exit(0)
+signal.signal(signal.SIGINT,signal_handler)
+signal.pause()
