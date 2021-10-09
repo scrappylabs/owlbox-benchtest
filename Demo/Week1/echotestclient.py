20a309c3b8891c1383a9d1b5235c32765fb208b1
PremKSU <pkurumpa@students.kennesaw.edu>
2021-10-09 17:59:40 -0400

    Updated Tests

--- Demo/Week1/echotestclient.py
+++ Demo/Week1/echotestclient.py
@@ -1,11 +1,11 @@
 import socket 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print "Socket Created"
-host = raw_input("HOST >>>   ")
+print ("Socket Created")
+host = int(raw_input("HOST >>>   "))
 
 # 3. Use the same port no. entered on server.py as the client binds to the 
 #    same port
-port = input ("Enter the PORT number (1 - 10,000)") 
+port = int(input ("Enter the PORT number (1 - 10,000)")) 
 s.connect((host, port))
 
 while True:   
