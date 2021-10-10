7ed5d868c7a4e57937c708fd056f72f2258cf013
PremKSU <pkurumpa@students.kennesaw.edu>
2021-10-10 14:38:07 -0400

    Working LED Strip Demo
    TX OwlBox: pytxeth
    RX OwlBox: pyrxeth
    RasPi: py_RasPi_RX_Test

---
+++ Demo/Week1/py_RasPi_RX_Test.py
@@ -0,0 +1,92 @@
+import sys
+import socket
+import signal
+import time
+from rpi_ws281x import *
+import argparse
+
+import threading
+
+
+velocity = 0
+pktdata = "5554004"
+
+
+LED_COUNT      = 16      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+ETH_P_ALL=0x88DC # not defined in socket module, sadly...
+s=socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+s.bind(("eth0", 0))
+pktnum = 1
+
+
+#Strip Initialization
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+def loop_pkt():
+    pktnumrcv = 0
+    global velocity
+    global pktdata
+    while True:
+        global data
+        global velocity
+        #print(velocity)
+        r=s.recv(2000)
+        print("Received packet %i" % pktnumrcv)
+        pktnumrcv = pktnumrcv + 1
+        velocity = int(str(r).strip()[-2:-1])
+        print(velocity)
+        #print(str(r[0]).strip()[-2:])
+        pktdata = r
+        #print("%s" % r)
+
+def parse_args():
+    global velocity
+    global pktdata
+    while True:
+        internalvelocity = int(pkt.strip()[-2:-1])
+        velocity = internalvelocity
+        print("internal velocity: ", internalvelocity)
+
+
+def update_LED():
+    global velocity
+    global pktdata
+    while True:
+        #if (velocity > 200):
+            #velocity = 0
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, Color(velocity*20, 0, 0))
+            strip.show()
+            time.sleep(2/1000)
+        #velocity = velocity + 1
+        print("velocity ", velocity)
+        
+
+
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=loop_pkt)
+    t2 = threading.Thread(target=parse_args)
+    t3 = threading.Thread(target=update_LED)
+
+t1.start()
+#t2.start()
+t3.start()
+
+
+def signal_handler(sig, frame):
+	#print('\r\nNumber Rx: ')
+	#print('\r\n'+len(pktdata))
+	sys.exit(0)
+signal.signal(signal.SIGINT,signal_handler)
+signal.pause()
---
+++ Demo/Week1/pytxeth.py
@@ -0,0 +1,76 @@
+from socket import socket, AF_PACKET, SOCK_RAW
+from scapy.all import hexdump, raw, bytes_encode
+from scapy.layers.l2 import Ether, Dot1Q
+
+import time
+from datetime import datetime, timedelta
+
+t_cam = "\xFF\xFF\xFF\xFF\xFF\xFF\x74\xE5\x43\xD5\xFD\xA4\x89\x47\x01\x00\xF1\x01\x20\x50\x02\x74\x00\x2D\x01\x00\x95\xF9\x74\xE5\x43\xD5\xFD\xA4\xA7\x84\x11\x99\x19\x54\x08"
+
+t_cam2 = "\x02\x20\x0f\x01\xac\x10\x01\x0c\x04\x01\x05\x80\x00\x31\x02\x00\x30\x2d\x80\x01\x02\x81\x26\x14\x80\x22\x22\x0e\x8f\xc0\x14\x2a\x28\x27\xcd\x94\xdc\x70\x0a\xb9\xff\xff\xff\xff\xe0\x02\x70\x80\x7f\x00\x08\x07\xd1\x81\x7f\xff\x08\x00\x0c\x80\x32\xa3\x00"
+
+t_cam3 = "\xc3\xbf\xc3\xbf\xcf\xbf\x74\xE5\x43\xD5\xFD\xA4"
+
+t_cam5 = "\x02\x20\x0f\x01\xac\x10\x01\x06\x04\x01\x0e\x80\x00\x31\x02\x00\x30\x2d\x80\x01\x02\x81\x26\x00\x95\xb9\x6b\xfe\xff\xff\x35\xa4\xe9\x01\x6b\x49\xd2\x01\xf0\x00\xff\xff\xff\xff\xff\xff\x70\x80\x7f\x07\xd1\x07\xd1\x81\x7f\xff\x08\x00\x0c\x80\x32\xa3\x00\x70\x72\x65\x6d\x34\x34\x30\x30\x70\x72\x65\x6d"
+#t_cam5 = t_cam5.enc(ASN1_Codecs.BER)
+number = 30
+
+#pkt = #pkt/"\x02\x20\x0f\x01\xac\x10\x01\x06\x04\x01\x0e\x80\x00\x31\x02\x00\x30\x2d\x80\x01\x02\x81\x26\x00\x95\xb9\x6b\xfe\xff\xff\x35\xa4\xe9\x01\x6b\x49\xd2\x01\xf0\x00\xff\xff\xff\xff\xff\xff\x70\x80\x7f\x07\xd1\x07\xd1\x81\x7f\xff\x08\x00\x0c\x80\x32\xa3\x00"
+#pkt = pkt/"\x02\x20\x0f\x01\xac\x10\x01\x06\x04\x01\x0e\x80\x00\x31\x02\x00\x30\x2d\x80\x01\x02\x81\x26\x00\x95\xb9\x6b\xfe\xff\xff\x35\xa4\xe9\x01\x6b\x49\xd2\x01\xf0\x00\xff\xff\xff\xff\xff\xff\x70\x80\x7f\x07\xd1\x07\xd1\x81\x7f\xff\x08\x00\x0c\x80\x32\xa3\x00"
+def packetcreate():
+    global encoded
+    data = t_cam5 + str(number)
+    pkt = Ether()
+    pkt[Ether].dst = "ff:ff:ff:ff:ff:ff"
+    pkt[Ether].src = "7c:e9:3:51:86:6e"
+    pkt[Ether].type = 0x88dc
+    pkt = pkt/data
+    encoded = bytes_encode(pkt)
+
+s = socket(AF_PACKET, SOCK_RAW)
+s.bind(("wlp1s0", 0))
+# We're putting together an ethernet frame here, 
+# but you could have anything you want instead
+# Have a look at the 'struct' module for more 
+# flexible packing/unpacking of binary data
+# and 'binascii' for 32 bit CRC
+#src_addr = "\x7C\xE9\x03\x51\x86\x6E"
+#dst_addr = "\xFF\xFF\xFF\xFF\xFF\xFF"
+#payload = ("["*30)+"PAYLOAD"+("]"*30)
+#checksum = "\x1a\x2b\x3c\x4d"
+#ethertype = "\x88\xDC"
+#message = dst_addr+src_addr+ethertype+payload
+
+
+pktnum = 1
+print("Enter send speed (Hz) - Enter '2000' for maximum transfer rate")
+speed = int(input())
+speedHz = (1/speed)
+print("Enter duration of test(s)")
+times = int(input())
+start_time = time.ctime()
+end_time = datetime.now() + timedelta(microseconds=(times*1000000)) 
+inttimes = int(times)
+print("Sending for %i seconds..." % times)
+
+while datetime.now() < end_time:
+    packetcreate()
+    number = number + 1
+    if(number == 39):
+        number = 30
+    if(speed == 2000):
+     s.send(encoded)
+     pktnum = pktnum + 1
+    else:
+     time.sleep(speedHz)
+     s.send(encoded)
+     pktnum = pktnum + 1
+     #print(hexdump(raw(encoded)))
+     #print("sent packet number: " + str(pktnum))
+
+print(">>Start Time: %s" % start_time)
+print(">>End Time: %s" % time.ctime())
+print(">>Time Spent Running: %s seconds" % times)
+print(">>Total packets sent: %s" % pktnum)
+print(">>Current speed: %i pkts/sec" % (pktnum/inttimes))
+#print(speedHz)
